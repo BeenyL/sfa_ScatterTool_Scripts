7608768e4815be9b14eefe247238d1408a6a48b7
Chengheng Liang <liangchengheng1@gmail.com>
2020-10-25 23:34:27 -0500

    Added sub division base, optimized combBox output values

--- src/scatter.py
+++ src/scatter.py
@@ -47,24 +47,41 @@ class ScatterUI(QtWidgets.QDialog):
         self.scatter_btn.clicked.connect(self.scatter_object)
         self.cancel_btn.clicked.connect(self.cancel)
         self.shape_btn.clicked.connect(self.create_shape)
-        self.update_div_btn.clicked.connect(self.update_div)
+        self.sub_ax_sbx.valueChanged.connect(self.update_sub_val_ax)
+        self.sub_hgt_sbx.valueChanged.connect(self.update_sub_val_hgt)
+        self.sub_bas_sbx.valueChanged.connect(self.update_sub_val_bas)
+        self.shape_cmb.currentIndexChanged.connect(self.update_div)
+
+    def update_sub_val_ax(self):
+        self.defaultSubDiv.cur_sub_ax = self.sub_ax_sbx.value()
+
+    def update_sub_val_hgt(self):
+        self.defaultSubDiv.cur_sub_hgt = self.sub_hgt_sbx.value()
+
+    def update_sub_val_bas(self):
+        self.defaultSubDiv.cur_sub_bas = self.sub_bas_sbx.value()
 
     @QtCore.Slot()
     def create_shape(self):
-        self.defaultSubDiv.cur_sub_ax = self.sub_ax_sbx.value()
-        self.defaultSubDiv.cur_sub_hgt = self.sub_hgt.sbx.value()
         if self.shape_cmb.currentText() == "Cube":
-            cmds.polyCube(name="Cube", sa=self.defaultSubDiv.cur_sub_ax,
-                          sh=self.defaultSubDiv.cur_sub_hgt)
+            cmds.polyCube(name="Cube",
+                          sw=self.defaultSubDiv.cur_sub_ax,
+                          sh=self.defaultSubDiv.cur_sub_hgt,
+                          sd=self.defaultSubDiv.cur_sub_bas)
         if self.shape_cmb.currentText() == "Sphere":
-            cmds.polySphere(name="Sphere", sa=self.defaultSubDiv.cur_sub_ax,
+            cmds.polySphere(name="Sphere",
+                            sa=self.defaultSubDiv.cur_sub_ax,
                             sh=self.defaultSubDiv.cur_sub_hgt)
         if self.shape_cmb.currentText() == "Cylinder":
-            cmds.polyCylinder(name="Cylinder", sa=self.defaultSubDiv.cur_sub_ax
-                              , sh=self.defaultSubDiv.cur_sub_hgt)
+            cmds.polyCylinder(name="Cylinder",
+                              sa=self.defaultSubDiv.cur_sub_ax,
+                              sh=self.defaultSubDiv.cur_sub_hgt,
+                              sc=self.defaultSubDiv.cur_sub_bas)
         if self.shape_cmb.currentText() == "Cone":
-            cmds.polyCone(name="Cone", sa=self.defaultSubDiv.cur_sub_ax,
-                          sh=self.defaultSubDiv.cur_sub_hgt)
+            cmds.polyCone(name="Cone",
+                          sa=self.defaultSubDiv.cur_sub_ax,
+                          sh=self.defaultSubDiv.cur_sub_hgt,
+                          sc=self.defaultSubDiv.cur_sub_bas)
 
     @QtCore.Slot()
     def scatter_object(self):
@@ -82,22 +99,26 @@ class ScatterUI(QtWidgets.QDialog):
     @QtCore.Slot()
     def update_div(self):
         """update subdivision"""
-
         if self.shape_cmb.currentText() == "Cube":
             self.defaultSubDiv.cur_sub_ax = self.defaultSubDiv.cb_sub_ax
             self.defaultSubDiv.cur_sub_hgt = self.defaultSubDiv.cb_sub_hgt
+            self.defaultSubDiv.cur_sub_bas = self.defaultSubDiv.cb_sub_bas
         if self.shape_cmb.currentText() == "Sphere":
             self.defaultSubDiv.cur_sub_ax = self.defaultSubDiv.s_sub_ax
             self.defaultSubDiv.cur_sub_hgt = self.defaultSubDiv.s_sub_hgt
+            self.defaultSubDiv.cur_sub_bas = self.defaultSubDiv.s_sub_bas
         if self.shape_cmb.currentText() == "Cylinder":
             self.defaultSubDiv.cur_sub_ax = self.defaultSubDiv.cyl_sub_ax
             self.defaultSubDiv.cur_sub_hgt = self.defaultSubDiv.cyl_sub_hgt
+            self.defaultSubDiv.cur_sub_bas = self.defaultSubDiv.cyl_sub_bas
         if self.shape_cmb.currentText() == "Cone":
             self.defaultSubDiv.cur_sub_ax = self.defaultSubDiv.cn_sub_ax
             self.defaultSubDiv.cur_sub_hgt = self.defaultSubDiv.cn_sub_hgt
+            self.defaultSubDiv.cur_sub_bas = self.defaultSubDiv.cn_sub_bas
 
         self.sub_ax_sbx.setValue(self.defaultSubDiv.cur_sub_ax)
         self.sub_hgt_sbx.setValue(self.defaultSubDiv.cur_sub_hgt)
+        self.sub_bas_sbx.setValue(self.defaultSubDiv.cur_sub_bas)
 
     @QtCore.Slot()
     def cancel(self):
@@ -110,18 +131,28 @@ class ScatterUI(QtWidgets.QDialog):
         self.shape_cmb.addItems(['Cube', 'Sphere', 'Cylinder', 'Cone'])
         self.shape_btn = QtWidgets.QPushButton("Create Shape")
         self.update_div_btn = QtWidgets.QPushButton("Update Division")
+
         self.sub_ax_sbx = QtWidgets.QSpinBox()
         self.sub_ax_sbx.setButtonSymbols(QtWidgets.QAbstractSpinBox.PlusMinus)
         self.sub_ax_sbx.setFixedWidth(50)
         self.sub_ax_sbx.setValue(self.defaultSubDiv.cur_sub_ax)
+
         self.sub_hgt_sbx = QtWidgets.QSpinBox()
         self.sub_hgt_sbx.setButtonSymbols(QtWidgets.QAbstractSpinBox.PlusMinus)
         self.sub_hgt_sbx.setFixedWidth(50)
         self.sub_hgt_sbx.setValue(self.defaultSubDiv.cur_sub_hgt)
+
+        self.sub_bas_sbx = QtWidgets.QSpinBox()
+        self.sub_bas_sbx.setButtonSymbols(QtWidgets.QAbstractSpinBox.PlusMinus)
+        self.sub_bas_sbx.setFixedWidth(50)
+        self.sub_bas_sbx.setValue(self.defaultSubDiv.cur_sub_bas)
+
         self.sub_ax = QtWidgets.QLabel("Sub Axis")
         self.sub_ax.setFixedWidth(100)
         self.sub_hgt = QtWidgets.QLabel("Sub Height")
         self.sub_hgt.setFixedWidth(100)
+        self.sub_bas = QtWidgets.QLabel("Sub Base")
+        self.sub_bas.setFixedWidth(100)
         layout = QtWidgets.QHBoxLayout()
         layout.addWidget(self.shape_cmb, 0, 0)
         layout.addWidget(self.shape_btn, 0, 1)
@@ -130,6 +161,8 @@ class ScatterUI(QtWidgets.QDialog):
         layout.addWidget(self.sub_ax_sbx, 0, 4)
         layout.addWidget(self.sub_hgt, 0, 5)
         layout.addWidget(self.sub_hgt_sbx, 0, 6)
+        layout.addWidget(self.sub_bas, 0, 7)
+        layout.addWidget(self.sub_bas_sbx, 0, 8)
         return layout
 
     def sct_cnl_layout_ui(self):
@@ -146,11 +179,16 @@ class DefaultSubDiv(object):
     def __init__(self):
         self.cur_sub_ax = 0
         self.cur_sub_hgt = 0
-        self.cb_sub_ax = 4
-        self.cb_sub_hgt = 4
+        self.cur_sub_bas = 0
+        self.cb_sub_ax = 1
+        self.cb_sub_hgt = 1
+        self.cb_sub_bas = 1
         self.s_sub_ax = 8
         self.s_sub_hgt = 8
+        self.s_sub_bas = 0
         self.cyl_sub_ax = 8
         self.cyl_sub_hgt = 4
+        self.cyl_sub_bas = 0
         self.cn_sub_ax = 8
         self.cn_sub_hgt = 3
+        self.cn_sub_bas = 0
